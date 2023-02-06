87373716f7ffd94277274aee3161327f97d30716
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-02-06 22:58:15 +0530

    [Sartaj] : added read write CSV file program

---
+++ CsvAndJson/CSVHandler.cs
@@ -0,0 +1,51 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvAndJson
+{
+    class CSVHandler
+    {
+        public static void ImplementCSVDataHandling()
+        {
+            string importFilePath = @"C:\Users\Sartaj khan\source\repos\CsvAndJson\CsvAndJson\AddressData.csv";
+            string exportFilePath = @"C:\Users\Sartaj khan\source\repos\CsvAndJson\CsvAndJson\Export.csv";
+
+            //reading csv file
+            using (var reader = new StreamReader(importFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressData>().ToList();
+                Console.WriteLine("Read data successfully from addresses csv.");
+                foreach (AddressData addressData in records)
+                {
+                    Console.Write("\t" + addressData.firstname);
+                    Console.Write("\t" + addressData.lastname);
+                    Console.Write("\t" + addressData.address);
+                    Console.Write("\t" + addressData.city);
+                    Console.Write("\t" + addressData.state);
+                    Console.Write("\t" + addressData.code);
+                    Console.WriteLine();
+
+
+                }
+                Console.WriteLine("**********************Reading fromcsv file and Write to csv file **************************");
+                //Writing csv file
+
+                using (var writer = new StreamWriter(exportFilePath))
+                using (var csvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvExport.WriteRecords(records);
+                }
+                Console.WriteLine("Record inserted successfully in file in CSV format");
+            }
+        }
+
+
+    }
+
+}
